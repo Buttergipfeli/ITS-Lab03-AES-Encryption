21016c991a699d815c75c5ebbaae09a3b8d573ed
Andre Kocher <77845220+Buttergipfeli@users.noreply.github.com>
2025-10-03 19:20:49 +0200

    handle empty strings in AESIVWithTagEncryptionService as exceptions

--- ITS Lab03 AES Encryption/Model/CryptoError.swift
+++ ITS Lab03 AES Encryption/Model/CryptoError.swift
@@ -1,6 +1,5 @@
 import CommonCrypto
 
 public enum CryptoError: Error {
-    case invalidBase64, nonUTF8, badKeyLength, badIVLength, ccmUnavailable
-    case ccError(CCCryptorStatus)
+    case decodingFailed
 }
--- ITS Lab03 AES Encryption/Service/AESIVWithTagEncryptionService.swift
+++ ITS Lab03 AES Encryption/Service/AESIVWithTagEncryptionService.swift
@@ -14,13 +14,16 @@ final class AESIVWithTagEncryptionService: AESIVWithTagEncryptionServiceProtocol
     }
     
     func decrypt(encryptedText: String, mode: AESIVWithTag, key: String) throws -> String {
-        guard let cipherData = Data(base64Encoded: encryptedText) else { return "" }
+        guard let cipherData = Data(base64Encoded: encryptedText) else { throw CryptoError.decodingFailed }
         let cipherBytes = [UInt8](cipherData)
         let blockMode = mode.blockMode(messageLength: key.bytes.count, mode: .decrypt)
         let aes = try AES(key: key.bytes, blockMode: blockMode, padding: .noPadding)
         let decryptedBytes = try aes.decrypt(cipherBytes)
-        let plaintext = String(bytes: decryptedBytes, encoding: .utf8)
         
-        return plaintext ?? ""
+        if let plainText = String(bytes: decryptedBytes, encoding: .utf8) {
+            return plainText
+        } else {
+            throw CryptoError.decodingFailed
+        }
     }
 }

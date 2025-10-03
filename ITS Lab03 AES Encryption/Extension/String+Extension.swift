fed4f7a2d47ff55938847a9577ef4c4a180ba122
Andre Kocher <77845220+Buttergipfeli@users.noreply.github.com>
2025-10-03 19:54:26 +0200

    remove unused code

--- ITS Lab03 AES Encryption/Extension/String+Extension.swift
+++ ITS Lab03 AES Encryption/Extension/String+Extension.swift
@@ -5,19 +5,10 @@ extension String {
         prefix(1).uppercased() + dropFirst()
     }
     
-    var base64Encoded: String {
-        Data(self.utf8).base64EncodedString()
-    }
-    
     func sanitizeToUTF8() -> String {
         self.unicodeScalars
             .filter { $0.isASCII && $0.value >= 32 && $0.value < 127 }
             .map { String($0) }
             .joined()
-    }
-    
-    var base64Decoded: String? {
-        guard let data = Data(base64Encoded: self) else { return nil }
-        return String(data: data, encoding: .utf8)
     }
 }
